8ba6c82ea906e9fd877ae467c6c3ba8018ad0870
thanhthudhbkhn <thanhthuk59@gmail.com>
2017-05-11 20:08:38 +0700

    tim kiem co in ket qua

--- src/timkiemxau/Search.java
+++ src/timkiemxau/Search.java
@@ -5,19 +5,35 @@ import java.util.ArrayList;
 public class Search {
 
     public static void main(String[] args) {
-        String content = "con    mèo hôm nay đi dạo. Lọ đường. Rồi đi chơi. Bác đang dạy các con học bài";
-        String keywords = "mèo đang học";
-        System.out.println( search(content,keywords) );
+        String content = "con mèo hôm nay đi dạo. Lọ đường. Rồi đi chơi. Bác đang dạy các con học bài";
+        String keywords = "con đang học";
+        ArrayList<String> result = new ArrayList<>();
+        //xâu keyword có thuộc content không? nếu có in ra result theo y/c của thầy
+        if(content.contains(keywords)==true) result = splitKeywords(keywords);
+        else result = search(content, keywords);
+        
+        if (result!=null){
+	        for (int i=0;i<result.size();i++) {
+	            if (!result.get(i).equals("")) {
+	                if (i%2==0) System.out.print(result.get(i)+" ");
+	                else {
+	                	System.out.print(result.get(i));
+	                	 if(i<result.size()-1) System.out.print(" ... ");
+	                }
+	            }
+	        }
+        }
     }
 
-    private static  boolean search(String content, String keywords) {
+    private static  ArrayList<String> search(String content, String keywords) {
         ArrayList<String> listKeyword = splitKeywords(keywords);
         ArrayList<String> listContentWords = splitContent(content);
+        ArrayList<String> result = new ArrayList<>();
         int begin=0;
         for (int k = 0; k < listKeyword.size(); k++) {
             int check=0;
             for (int c = begin; c < listContentWords.size(); c++) {
-            	String tmpContentWords = listContentWords.get(c).trim().toLowerCase();
+            	String tmpContentWords = listContentWords.get(c).trim();
             	int i=tmpContentWords.length()-1;
         		//loai bo cac dau cau
             	if(i>=0){
@@ -28,15 +44,25 @@ public class Search {
         				if(i==-1) break;
         			}
         		}
+                //System.out.print(tmpContentWords);
             	if (listKeyword.get(k).equals(tmpContentWords)) {
                 	check=1; 
-                	begin=c+1;
+                	//System.out.println("listKeyword.get(k): "+listKeyword.get(k)+" listContentWords.get(c):"+listContentWords.get(c)+" check=1"+" c="+c);
+                    int match = c;
+                    if (match - 1 >= 0) {
+                        result.add(listContentWords.get(match-1));
+                        result.add(listContentWords.get(match));
+                    } else if (match + 1 < listContentWords.size()) {
+                        result.add(listContentWords.get(match));
+                        result.add(listContentWords.get(match+1));
+                    }
+                    begin=c;
                     break;
                 }
             }
-            if(check==0) return false;
+            if(check==0) {System.out.println("khong tim thay :( "); return null;}
         }
-        return true;
+        return result;
     }
 
     private static ArrayList<String> splitKeywords(String keywords) {
